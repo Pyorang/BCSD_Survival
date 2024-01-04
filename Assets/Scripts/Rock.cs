4f4c8e896e61326de9733904db89c8f57cc34895
Pyorang <sudangi143@gmail.com>
2024-01-04 22:44:23 +0900

    ver0.8

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -13,12 +13,19 @@ public class Rock : MonoBehaviour
     [SerializeField]
     private SphereCollider col; //구체 콜라이더
 
+    //필요한 게임 오브젝트.
     [SerializeField]
     private GameObject go_rock; //일반 바위
     [SerializeField]
     private GameObject go_debris;   // 깨진 바위
     [SerializeField]
     private GameObject go_effect_prefabs;   //채굴 이펙트.
+    [SerializeField]
+    private GameObject go_rock_item_prefab; //돌맹이 아이템
+
+    //돌맹이 아이템 등장 개수
+    [SerializeField]
+    private int count;
 
     //필요한 사운드 이름
     [SerializeField]
@@ -44,8 +51,11 @@ public class Rock : MonoBehaviour
         SoundManager.instance.PlaySE(destroy_Sound);
 
         col.enabled = false;
+        for(int i = 0; i < count; i++)
+        {
+            Instantiate(go_rock_item_prefab, go_rock.transform.position + new Vector3(0,1,0), Quaternion.identity);
+        }
         Destroy(go_rock);
-
         go_debris.SetActive(true);
         Destroy(go_debris, destroyTime);
     }
