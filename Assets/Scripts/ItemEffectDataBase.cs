7427f45b766139d3f5ae323cb8f148fd05f49e16
Pyorang <sudangi143@gmail.com>
2024-01-07 20:17:44 +0900

    ver0.83

--- Assets/Scripts/ItemEffectDataBase.cs
+++ Assets/Scripts/ItemEffectDataBase.cs
@@ -23,8 +23,20 @@ public class ItemEffectDataBase : MonoBehaviour
     private Status_Controller thePlayerStatus;
     [SerializeField]
     private WeaponManager theWeaponManager;
+    [SerializeField]
+    private SlotToolTip theSlotToolTip;
 
     private const string HP = "HP", SP = "SP", DP = "DP", HUNGRY = "HUNGRY", THIRSTY = "THIRSTY", SATISFY = "SATISFY";
+
+    public void ShowToolTip(Item _item, Vector3 _pos)
+    {
+        theSlotToolTip.ShowToolTip(_item, _pos);
+    }
+
+    public void HideToolTip()
+    {
+        theSlotToolTip.HideToolTip();
+    }
 
     public void UseItem(Item _item)
     {
