ac88332a203595e2afd51d8fe4c4f808a5b837e0
Pyorang <sudangi143@gmail.com>
2024-01-10 15:12:22 +0900

    ver0.88

--- Assets/Scripts/NPC/Pig.cs
+++ Assets/Scripts/NPC/Pig.cs
@@ -2,103 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pig : MonoBehaviour
+public class Pig : WeakAnimal
 {
-    [SerializeField]
-    private string animalName;  //동물의 이름
-
-    [SerializeField]
-    private int hp;             //동물의 체력
-
-    [SerializeField]
-    private float walkSpeed;    //걷기 스피드
-    [SerializeField]
-    private float runSpeed;     //뛰기 스피드
-    private float applySpeed;   //
-
-    private Vector3 direction;
-
-    //상태변수
-    private bool isAction;      //행동중인지 아닌지 판별.
-    private bool isWalking;     //걷는지 안 걷는지 판별.
-    private bool isRunning;     //뛰는지 판별.
-    private bool isDead;        //죽었는지 판별.
-
-    [SerializeField]
-    private float walkTime;     //걷기 시간
-    [SerializeField]
-    private float waitTime;     //대기 시간
-    [SerializeField]
-    private float runTime;      //뛰기 시간.
-    private float currentTime;
-
-    //필요한 컴포넌트
-    [SerializeField]
-    private Animator anim;
-    [SerializeField]
-    private Rigidbody rigid;
-    [SerializeField]
-    private BoxCollider boxCol;
-    private AudioSource theAudio;
-
-    [SerializeField]
-    private AudioClip[] sound_pig_Normal;
-    [SerializeField]
-    private AudioClip sound_pig_hurt;
-    [SerializeField]
-    private AudioClip sound_pig_Dead;
-
-    // Start is called before the first frame update
-    void Start()
+    protected override void ResetAct()
     {
-        theAudio = GetComponent<AudioSource>();
-        currentTime = waitTime;
-        isAction = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(!isDead)
-        {
-            Move();
-            Rotation();
-            ElapseTime();
-        }
-    }
-
-    private void Move()
-    {
-        if (isWalking || isRunning)
-            rigid.MovePosition(transform.position + (transform.forward * applySpeed * Time.deltaTime));
-    }
-
-    private void Rotation()
-    { 
-        if(isWalking || isRunning)
-        {
-            Vector3 _rotation = Vector3.Lerp(transform.eulerAngles, new Vector3(0f, direction.y, 0f), 0.01f);
-            rigid.MoveRotation(Quaternion.Euler(_rotation));
-        }
-    }
-
-    private void ElapseTime()
-    {
-        currentTime -= Time.deltaTime;
-        if (currentTime <= 0)
-            ResetAct();
-
-    }
-
-    private void ResetAct()
-    {
-        isWalking = false;
-        isRunning = false;
-        isAction = true;
-        applySpeed = walkSpeed;
-        anim.SetBool("Walking", isWalking);
-        anim.SetBool("Running", isRunning);
-        direction.Set(0f, Random.Range(0f, 360f), 0f);
+        base.ResetAct();
         RandomAction();
     }
 
@@ -136,64 +44,5 @@ public class Pig : MonoBehaviour
         currentTime = waitTime;
         anim.SetTrigger("Peek");
         Debug.Log("두리번");
-    }
-
-    private void TryWalk()
-    {
-        isWalking = true;
-        anim.SetBool("Walking", isWalking);
-        currentTime = walkTime;
-        applySpeed = walkSpeed;
-        Debug.Log("걷기");
-    }
-
-    public void Run(Vector3 _targetPos)
-    {
-        direction = Quaternion.LookRotation(transform.position - _targetPos).eulerAngles;
-
-        currentTime = runTime;
-        isWalking = false;
-        isRunning = true;
-        applySpeed = runSpeed;
-        anim.SetBool("Running", isRunning);
-    }
-
-    public void Damage(int _dmg, Vector3 _targetPos)
-    {
-        if(!isDead)
-        {
-            hp -= _dmg;
-            if (hp <= 0)
-            {
-                Dead();
-                Debug.Log("체력 0 이하");
-                return;
-            }
-
-            PlaySE(sound_pig_hurt);
-            anim.SetTrigger("Hurt");
-            Run(_targetPos);
-        }
-    }
-
-    private void Dead()
-    {
-        PlaySE(sound_pig_Dead);
-        isWalking = false;
-        isRunning = false;
-        isDead = true;
-        anim.SetTrigger("Dead");
-    }
-
-    private void RandomSound()
-    {
-        int _random = Random.Range(0, 3);   //일상 사운드 3개
-        PlaySE(sound_pig_Normal[_random]);
-    }
-
-    private void PlaySE(AudioClip _clip)
-    {
-        theAudio.clip = _clip;
-        theAudio.Play();
     }
 }
--- Assets/Scripts/PickAxeController.cs
+++ Assets/Scripts/PickAxeController.cs
@@ -31,10 +31,10 @@ public class PickAxeController :CloseWeaponController
                 {
                     hitInfo.transform.GetComponent<Rock>().Mining();
                 }
-                else if (hitInfo.transform.tag=="NPC")
+                else if (hitInfo.transform.tag=="WeakAnimal")
                 {
                     SoundManager.instance.PlaySE("Animal_Hit");
-                    hitInfo.transform.GetComponent<Pig>().Damage(1, transform.position);
+                    hitInfo.transform.GetComponent<WeakAnimal>().Damage(1, transform.position);
                 }
                 isSwing = false;
                 Debug.Log(hitInfo.transform.name);
