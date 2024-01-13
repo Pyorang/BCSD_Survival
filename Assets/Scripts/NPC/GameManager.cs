91d812403ab7d3cceaeb4f236d680331e5f9f2f1
Pyorang <sudangi143@gmail.com>
2024-01-13 21:21:05 +0900

    ver0.97

--- Assets/Scripts/NPC/GameManager.cs
+++ Assets/Scripts/NPC/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour
     public static bool isNight = false;
     public static bool isWater = false;
 
+    public static bool isPause = false; //메뉴가 호출되면 true
+
     private WeaponManager theWM;
     private bool flag = false;
 
@@ -26,7 +28,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isOpenInventory || isOpenCraftManual)
+        if (isOpenInventory || isOpenCraftManual || isPause)
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
